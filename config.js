b53930e9fd87b195ce94c0c4ed0fa4c061c84866
Imperial-Gaming <72310939+Imperial-Gaming@users.noreply.github.com>
2021-09-25 17:48:27 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAjMY26GV5dk5GqqycpfhvEmi7Wnk-lbWk",
+    authDomain: "team-voting-app-3f8e4.firebaseapp.com",
+    projectId: "team-voting-app-3f8e4",
+    storageBucket: "team-voting-app-3f8e4.appspot.com",
+    messagingSenderId: "151605800130",
+    appId: "1:151605800130:web:1c71534f9d0c8901dedf5b",
+    measurementId: "G-G29E4RFHB2"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
